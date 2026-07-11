fd02da51c4fa861fbf0fb788fa97b7ce0fc85276
retheeshmohan01-sketch <retheeshmohan01@gmail.com>
2026-07-11 17:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def fb_webhook():
+    if request.method == 'GET':
+        mode = request.args.get('hub.mode')
+        token = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        
+        if mode == 'subscribe' and token == 'kvmpbot2516':
+            return challenge, 200
+        return 'Invalid verification token', 403
+        
+    return 'Bot Running', 200
